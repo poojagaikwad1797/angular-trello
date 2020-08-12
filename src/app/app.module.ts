dd595413bf9f5636e936b107d187ff99e43a3063
Pooja Gaikwad <gaikwadpooja1797@gmail.com>
2020-08-13 03:59:26 +0530

    create first component description

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardsComponent } from './cards/cards.component';
+import { DescriptionComponent } from './description/description.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardsComponent
+    CardsComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule,
