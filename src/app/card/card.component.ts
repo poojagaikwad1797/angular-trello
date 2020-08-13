b7b9b5d8f91d3fb63bdee66ef87bacbd910e3ce2
Pooja Gaikwad <gaikwadpooja1797@gmail.com>
2020-08-13 07:39:59 +0530

    writing some code

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input} from '@angular/core';
+import { CardDataService } from 'src/services/card-data.service';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  @Input() id: number;
+  myCard: any;
 
-  ngOnInit(): void {
+  cardList: any[] = [
+    {
+      id: 1,
+      title: 'Drink water!',
+      description: 'Drink 8 glasses of water! Stay hydrated, king!'
+    },
+    {
+      id: 2,
+      title: 'Read a book',
+      description: 'It can be any book, a technical book, or just for enjoyment.'
+    },
+    {
+      id: 3,
+      title: 'Finish Gym Module',
+      description: 'Make sure it includes proper routing.'
+    }
+  ];
+
+  constructor(private cardDataService: CardDataService) { }
+
+  ngOnInit() {
+    // this.myCard = this.cardList.find(card => card.id === this.id);
+    this.myCard = this.cardDataService.getCard();
   }
-
 }
+
